a58f415a6d31a0685410dbc1a23aacbab81bf578
def <n@uk>
2021-01-02 13:37:48 +0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -14,11 +14,11 @@ addLayer("j", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(.5)
+        mult = new Decimal(1)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
+        return new Decimal(10)
     },
     
     row: 0, // Row the layer is in on the tree (0 is the first row)
